cc9b95092ce964434c81dac5a0446853841fd64d
Antoine Dautrait <antoine.dautrait@gmail.com>
2024-11-28 12:23:31 +0100

    [ADD] app structure

---
+++ app/agents.py
@@ -0,0 +1,11 @@
+class Agent:
+    def __init__(self):
+        pass
+
+class BuyingAgent(Agent):
+    def __init__(self):
+        pass
+
+class SupplierAgent(Agent):
+    def __init__(self):
+        pass
---
+++ app/interactions.py
@@ -0,0 +1,7 @@
+class Coalition:
+    def __init__(self):
+        pass
+
+class Negotiation:
+    def __init__(self):
+        pass
